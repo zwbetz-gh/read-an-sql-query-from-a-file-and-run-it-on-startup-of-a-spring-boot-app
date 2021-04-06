47de2565acee8e5b8e93b18bca0b7a37e3e4f307
zwbetz-gh <zwbetz@gmail.com>
2021-04-05 22:51:20 -0500

    spring boot exit

--- src/main/java/com/example/demo/SqlQueryApplicationRunner.java
+++ src/main/java/com/example/demo/SqlQueryApplicationRunner.java
@@ -11,6 +11,8 @@ import com.google.common.io.Resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -23,7 +25,15 @@ import lombok.extern.log4j.Log4j2;
 public class SqlQueryApplicationRunner implements ApplicationRunner {
 
   @Autowired
+  private ApplicationContext applicationContext;
+  
+  @Autowired
   JdbcTemplate jdbcTemplate;
+
+  private void exit(final int exitCode) {
+    SpringApplication.exit(applicationContext, () -> exitCode);
+    System.exit(exitCode);
+  }
 
   private String readFile(final String relFilePath) throws IOException {
     final URL url = Resources.getResource(relFilePath);
@@ -37,10 +47,9 @@ public class SqlQueryApplicationRunner implements ApplicationRunner {
     final List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
 
     if (rows.size() > 0) {
-      for (Map<String, Object> row : rows) {
-        log.warn(row.toString());
-      }
-      log.warn("You have TODOs ...");
+      rows.forEach(row -> log.error(row.toString()));
+      log.error("You have TODOs ...");
+      exit(1);
     } else {
       log.info("Congrats, your TODO list is empty");
     }
